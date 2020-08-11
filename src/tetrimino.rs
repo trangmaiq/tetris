40ffadcc5bd4b0435c64d4bced38c36fee2c2f27
Trang Mai <trangmaiq@gmail.com>
2020-08-11 15:59:37 +0700

    Implement rotating and moving a tetrimino

--- src/tetrimino.rs
+++ src/tetrimino.rs
@@ -11,6 +11,82 @@ struct Tetrimino {
     current_state: u8,
 }
 
+impl Tetrimino {
+    fn rotate(&mut self, game_map: &[Vec<u8>]) {
+        // A bit longer, indeed. Since we can't be sure that
+        // the piece will be put where we want it to go, we need to make temporary variables
+        // and then check the possibilities. We use the temporary variables before going further.
+        let mut tmp_state = self.current_state + 1;
+        if tmp_state as usize >= self.states.len() {
+            tmp_state = 0;
+        }
+        // This line its own doesn't make much sense but it'll be very useful next:
+        // in case the piece cannot be placed where we want, we try to move it on the `x` axis
+        // to see if it'd work in some other place. It allows you to have a Tetris
+        // that is much more flexible and comfortable to play
+        let x_pos = [0, -1, 1, -2, 2, -3];
+        // With the explanations given previously, this loop should be really easy to understand.
+        // For each x shift, we check whether the piece can be placed there.
+        // If it works, we change the values of our tetrimino, otherwise we just continue.
+        // If no `x` shift worked, we just leave the function without doing anything.
+        for x in x_pos.iter() {
+            if self.test_position(game_map, tmp_state as usize, self.x + x, self.y) == true {
+                self.current_state = tmp_state;
+                self.x += *x;
+                break
+            }
+        }
+    }
+
+    // Now that we can rotate and test the position of a tetrimino,
+    // it'd be nice to actually move it as well
+    // (when the timer hoes to 0 and the tetrimino needs to go down, for example)
+    // If the tetrimino cannot move, we'll return a Boolean value
+    // to allow the caller to be aware of it.
+    // We don't check possible positions, just the one received. The reason is simple,
+    // contrary to a rotation, we can't move the tetrimino around when it receives a move instruction.
+    // Imaging asking the tetrimino to move to the right and it doesn't move,
+    // or worse, it moves to the left.
+    // We can't allow it and so we're not doing it.
+    //
+    // If we can put the tetrimino in a place, we update the position of the tetrimino and return true,
+    // otherwise, we do nothing other than return false.
+    fn change_position(&mut self, game_map: &[Vec<u8>], new_x: isize, new_y: usize) -> bool {
+        if self.test_position(game_map, self.current_state as usize, new_x, new_y) == true {
+            self.x = new_x as isize;
+            self.y = new_y;
+            true
+        } else {
+            false
+        }
+    }
+
+    // It seems important to explain why the game map became a &[Vec<u8>].
+    // When we send a non-mutable reference over a vector (Vec<T>),
+    // it is then dereferenced into a & &[T] slice,
+    // which is a constant `view` over the vector's content.
+    //
+    // We loop over every block of our `tetrimino`
+    // and check whether the block is free in the game map
+    // (by checking whether it is equal to 0) and if it isn't going out of the game map.
+    fn test_position(&self, game_map: &[Vec<u8>], tmp_state: usize, x: isize, y: usize) -> bool {
+        for decal_y in 0..4 {
+            for decal_x in 0..4 {
+                let x = x + decal_x;
+                if self.states[tmp_state][decal_y][decal_x as usize] != 0
+                    && (y + decal_y >= game_map.len()
+                    || x < 0
+                    || x as usize >= game_map[y + decal_y].len()
+                    ||game_map[y + decal_y][x as usize] != 0
+                ) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
 trait TetriminoGenerator {
     fn new() -> Tetrimino;
 }
